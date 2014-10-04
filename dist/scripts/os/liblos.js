822fff93cd8daf1b5ba147e82c50e709639c02e5
Jacob Leach <leach.jacob@gmail.com>
2014-10-03 22:06:18 -0400

    Put in putChar function signature

--- dist/scripts/os/liblos.js
+++ dist/scripts/os/liblos.js
@@ -3,12 +3,17 @@ var TSOS;
     var liblos = (function () {
         function liblos() {
         }
-        liblos.shutdown = function () {
-            _Kernel.shutdown();
+        liblos.putChar = function () {
+            //If executing already, save that state
+            //Update shell PCB with correct address and swap it in
         };
 
         liblos.clockTick = function () {
             _Kernel.clockTick();
+        };
+
+        liblos.shutdown = function () {
+            _Kernel.shutdown();
         };
         return liblos;
     })();
--- source/scripts/os/liblos.ts
+++ source/scripts/os/liblos.ts
@@ -2,14 +2,20 @@ module TSOS
 {
   export class liblos
   {
-    public static shutdown(): void
+    public static putChar(): void
     {
-      _Kernel.shutdown();
+      //If executing already, save that state
+      //Update shell PCB with correct address and swap it in
     }
 
     public static clockTick(): void
     {
       _Kernel.clockTick();
+    }
+    
+    public static shutdown(): void
+    {
+      _Kernel.shutdown();
     }
   }
 }
