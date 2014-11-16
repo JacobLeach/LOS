f43d423025d1d7204eec770e0f051d02aa0190b1
Jacob Leach <leach.jacob@gmail.com>
2014-11-16 17:34:20 -0500

    HDD done.

--- dist/scripts/host/hdd.js
+++ dist/scripts/host/hdd.js
@@ -3,6 +3,20 @@ var TSOS;
     var HDD = (function () {
         function HDD() {
         }
+        HDD.prototype.setBlock = function (track, sector, block, value) {
+            localStorage.setItem(track.toString() + sector.toString() + block.toString(), JSON.stringify(value));
+        };
+
+        HDD.prototype.getBlock = function (track, sector, block) {
+            var bytes = JSON.parse(localStorage.getItem(track.toString() + sector.toString() + block.toString()));
+            var toReturn = [];
+
+            for (var i = 0; i < bytes.length; i++) {
+                toReturn.push(new TSOS.Byte(bytes[i].value));
+            }
+
+            return toReturn;
+        };
         return HDD;
     })();
     TSOS.HDD = HDD;
--- source/scripts/host/hdd.ts
+++ source/scripts/host/hdd.ts
@@ -2,6 +2,22 @@ module TSOS
 {
   export class HDD
   {
-    
+    public setBlock(track: number, sector: number, block: number, value: Byte[]): void
+    {
+      localStorage.setItem(track.toString() + sector.toString() + block.toString(), JSON.stringify(value));
+    }
+
+    public getBlock(track: number, sector: number, block: number): Byte[]
+    {
+      var bytes = JSON.parse(localStorage.getItem(track.toString() + sector.toString() + block.toString()));
+      var toReturn = [];
+
+      for(var i = 0; i < bytes.length; i++)
+      {
+        toReturn.push(new Byte(bytes[i].value));
+      }
+
+      return toReturn;
+    }
   }
 }
