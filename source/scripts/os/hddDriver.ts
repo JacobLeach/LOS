6a8f8c80ed3c1affc24951b7064c880619146875
Jacob Leach <leach.jacob@gmail.com>
2014-11-19 10:25:07 -0500

    Format finished

---
+++ source/scripts/os/hddDriver.ts
@@ -0,0 +1,60 @@
+module TSOS
+{
+  export class HDDDriver
+  {
+    public static TRACKS      = 4;
+    public static SECTORS     = 8;
+    public static BLOCKS      = 8;
+    public static BLOCK_SIZE  = 64;
+
+    private hdd: HDD;
+
+    constructor(hdd: HDD)
+    {
+      this.hdd = hdd;
+    }
+
+    public format(): void
+    {
+      for(var i = 0; i < HDDDriver.TRACKS; i++)
+      {
+        for(var j = 0; j < HDDDriver.SECTORS; j++)
+        {
+          for(var k = 0; k < HDDDriver.BLOCKS; k++)
+          {
+            this.hdd.setBlock(i, j, k, this.formattedBlock());
+          }
+        }
+      }
+    }
+
+    public createFile(name: string): boolean
+    {
+      return false;
+    }
+
+    public deleteFile(name: string): boolean
+    {
+      return false;
+    }
+
+    public readFile(name: string): boolean
+    {
+      return false;
+    }
+
+    //******************************** Helper Funnctions ******************************** 
+
+    private formattedBlock(): Byte[]
+    {
+      var block: Byte[] = [];
+
+      for(var i = 0; i < HDDDriver.BLOCK_SIZE; i++)
+      {
+        block[i] = new Byte(0);
+      }
+
+      return block;
+    }
+  }
+}
